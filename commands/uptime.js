bfbc35660421e3e75dca64bef2a71b62c6953c2f
Gusstx19 <78054851+Gusstx19@users.noreply.github.com>
2022-01-26 01:31:42 -0300

    Create uptime.js

---
+++ commands/uptime.js
@@ -0,0 +1,28 @@
+const Discord = require("discord.js");
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+  name: "uptime",
+  aliases: ["u"],
+  run: async(client, message, args) => {
+    
+    let days = Math.floor(client.uptime / 86400000 );
+    let hours = Math.floor(client.uptime / 3600000 ) % 24;
+    let minutes = Math.floor(client.uptime / 60000) % 60;
+    let seconds = Math.floor(client.uptime / 1000) % 60;
+    
+    let upembed = new Discord.MessageEmbed()
+    .setColor("GREEN")
+    .setDescription(`Olá ${message.author},`)
+    .addFields(
+        {
+            name: 'Aqui está o meu tempo online:',
+            value: `**${days}d ${hours}h ${minutes}m ${seconds}s**`,
+        },
+    )
+
+    
+    message.channel.send(upembed)
+    }
+  
+}
