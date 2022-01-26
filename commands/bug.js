cddba9426b265e47fb2f07bd157dfb9c70c4c1fc
Gusstx19 <78054851+Gusstx19@users.noreply.github.com>
2022-01-26 03:04:19 -0300

    Create bug.js and feedback.js

---
+++ commands/bug.js
@@ -0,0 +1,24 @@
+const Discord = require('discord.js')
+const { feedbackId } = require('../data/channelIDs.json')
+
+module.exports = {
+    name: "bug",
+    description: "bug command (embed style)",
+
+    async run (bot, message, args) {
+
+        if(!args[0]) return message.reply('Por favor coloque o motivo do seu report-bug!')
+
+        message.reply(`✉ | Obrigado pelo report! :)`)
+
+        const embed = new Discord.MessageEmbed()
+        .setColor("RANDOM")
+        .setTitle(`${message.author.username}#${message.author.discriminator}, Enviou um report-bug:`)
+        .setDescription(`${args}`)
+        .addField("Server Destinatário:", `${message.guild.name}`)
+        .addField("Server ID:", `${message.guild.id}`)
+
+        
+        bot.channels.cache.get(feedbackId).send(embed)
+    }
+}
---
+++ commands/feedback.js
@@ -0,0 +1,24 @@
+const Discord = require('discord.js')
+const { feedbackId } = require('../data/channelIDs.json')
+
+module.exports = {
+    name: "feedback",
+    description: "feedback command (embed style)",
+
+    async run (bot, message, args) {
+
+        if(!args[0]) return message.reply('Por favor coloque um razão em seu feedback!')
+
+        message.reply(`✉ | Obrigado pelo feedback! :)`)
+
+        const embed = new Discord.MessageEmbed()
+        .setColor("RANDOM")
+        .setTitle(`${message.author.username}#${message.author.discriminator}, Enviou um Feedback:`)
+        .setDescription(`${args}`)
+        .addField("Server Destinatário:", `${message.guild.name}`)
+        .addField("Server ID:", `${message.guild.id}`)
+
+        
+        bot.channels.cache.get(feedbackId).send(embed)
+    }
+}
