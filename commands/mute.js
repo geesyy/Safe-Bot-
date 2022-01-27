9484d49378a461071a563ac88e1a1fbc7503730e
Gusstx19 <78054851+Gusstx19@users.noreply.github.com>
2022-01-27 00:39:18 -0300

    Update mute.js

--- commands/mute.js
+++ commands/mute.js
@@ -7,13 +7,13 @@ module.exports = {
      * @param {Message} message
      */
     run : async(client, message, args) => {
-        if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.channel.send('Você não tem permissão para executar o comando.')
+        if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.channel.send('Você não tem permissão para executar o comando.').then((m) => m.delete({ timeout: 5000 }));
         const Member = message.mentions.members.first() || message.guild.members.cache.get(args[0])
-        if(!Member) return message.channel.send('Membro não encontrado.')
+        if(!Member) return message.channel.send('Membro não encontrado.').then((m) => m.delete({ timeout: 5000 }));
         const role = message.guild.roles.cache.find(role => role.name.toLowerCase() === 'muted')
         if(!role) {
             try {
-                message.channel.send('Muted role is not found, attempting to create muted role.')
+                message.channel.send('Muted role is not found, attempting to create muted role.').then((m) => m.delete({ timeout: 5000 }));
 
                 let muterole = await message.guild.roles.create({
                     data : {
@@ -27,7 +27,7 @@ module.exports = {
                         ADD_REACTIONS: false
                     })
                 });
-                message.channel.send('Muted role has sucessfully been created.')
+                message.channel.send('Muted role has sucessfully been created.').then((m) => m.delete({ timeout: 5000 }));
             } catch (error) {
                 console.log(error)
             }
@@ -35,6 +35,6 @@ module.exports = {
         let role2 = message.guild.roles.cache.find(r => r.name.toLowerCase() === 'muted')
         if(Member.roles.cache.has(role2.id)) return message.channel.send(`${Member.displayName} foi mutado com sucesso.`)
         await Member.roles.add(role2)
-        message.channel.send(`${Member.displayName} é o novo silenciado.`)
+        message.channel.send(`${Member.displayName} foi silenciado.`).then((m) => m.delete({ timeout: 5000 }));
     }
 }
