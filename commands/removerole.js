d6e66d9a06e1786ad561b8027025e03e02d5bf20
Gusstx19 <78054851+Gusstx19@users.noreply.github.com>
2022-01-27 00:01:43 -0300

    Update removerole.js

--- commands/removerole.js
+++ commands/removerole.js
@@ -1,11 +1,12 @@
 const discord = require("discord.js")
 
 module.exports.run = async (bot, message, args) => {
+    if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.channel.send('Você não tem permissão para usar este comando.').then((m) => m.delete({ timeout: 5000 }));
     let user = message.mentions.members.first()
-    if(!user) return message.channel.send("Por favor, especifique um usuário existente.")
+    if(!user) return message.channel.send("Por favor, especifique um usuário existente.").then((m) => m.delete({ timeout: 5000 }));
 
     let role = message.mentions.roles.first()
-    if(!role) return message.channel.send("Por favor, descreva uma tag existente.")
+    if(!role) return message.channel.send("Por favor, descreva uma tag existente.").then((m) => m.delete({ timeout: 5000 }));
 
     message.channel.send(`${user} foi retirado da tag ${role} com sucesso.`)
     user.roles.remove(role)
