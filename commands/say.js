00ace61b11ca326dbf82f613f03feb16262a4244
Gusstx19 <78054851+Gusstx19@users.noreply.github.com>
2022-01-27 00:39:22 -0300

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
  
 exports.run = async (bot, message, args) => {
   const sayMessage = args.join(' ');
-  if (!message.member.hasPermission("ADMINISTRATOR")) return message.reply(`🚨 | Você não tem permissão para isso.`)
+  if (!message.member.hasPermission("ADMINISTRATOR")) return message.reply(`você não tem permissão para isso.`).then((m) => m.delete({ timeout: 5000 }));
   message.delete().catch(O_o => {});
   message.channel.send(sayMessage);
 };
