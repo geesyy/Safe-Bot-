4c660d4e1381185fc14d04280b6389977b1b6205
Gusstx19 <78054851+Gusstx19@users.noreply.github.com>
2022-01-27 00:39:26 -0300

    Update unmute.js

--- commands/unmute.js
+++ commands/unmute.js
@@ -8,12 +8,12 @@ module.exports=  {
     run : async(client, message, args) => {
         const Member = message.mentions.members.first() || message.guild.members.cache.get(args[0])
 
-        if(!Member) return message.channel.send('Membro não encontrado')
+        if(!Member) return message.channel.send('Membro não encontrado').then((m) => m.delete({ timeout: 5000 }));
 
         const role = message.guild.roles.cache.find(r => r.name.toLowerCase() === 'muted');
 
         await Member.roles.remove(role)
 
-        message.channel.send(`${Member.displayName} é o mais novo desmutado.`)
+        message.channel.send(`${Member.displayName} está desmutado.`).then((m) => m.delete({ timeout: 5000 }));
     }
 }
