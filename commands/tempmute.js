17bc6cd37ad9746cca0303b63463adee474de2db
Gusstx19 <78054851+Gusstx19@users.noreply.github.com>
2022-01-27 00:39:24 -0300

    Update tempmute.js

--- commands/tempmute.js
+++ commands/tempmute.js
@@ -7,15 +7,15 @@ module.exports = {
      * @param {Message} message
      */
     run : async(client, message, args) => {
-        if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.channel.send('Você não tem permissão para usar este comando.')
+        if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.channel.send('Você não tem permissão para usar este comando.').then((m) => m.delete({ timeout: 5000 }));
         const Member = message.mentions.members.first() || message.guild.members.cache.get(args[0])
         const time = args[1]
-        if(!Member) return message.channel.send('Perdão, membro não encontrado.')
-        if(!time) return message.channel.send('Por favor, especifique um tempo para silenciar..')
+        if(!Member) return message.channel.send('Perdão, membro não encontrado.').then((m) => m.delete({ timeout: 5000 }));
+        if(!time) return message.channel.send('Por favor, especifique um tempo para silenciar.').then((m) => m.delete({ timeout: 5000 }));
         const role = message.guild.roles.cache.find(role => role.name.toLowerCase() === 'muted')
         if(!role) {
             try {
-                message.channel.send('A tag Muted não foi encontrada.')
+                message.channel.send('A tag Muted não foi encontrada.').then((m) => m.delete({ timeout: 5000 }));
 
                 let muterole = await message.guild.roles.create({
                     data : {
@@ -29,19 +29,19 @@ module.exports = {
                         ADD_REACTIONS: false
                     })
                 });
-                message.channel.send('A tag muted foi criada com sucesso, disponha.')
+                message.channel.send('A tag muted foi criada com sucesso, disponha.').then((m) => m.delete({ timeout: 5000 }));
             } catch (error) {
                 console.log(error)
             }
         };
         let role2 = message.guild.roles.cache.find(r => r.name.toLowerCase() === 'muted')
-        if(Member.roles.cache.has(role2.id)) return message.channel.send(`${Member.displayName} ja foi silenciado.`)
+        if(Member.roles.cache.has(role2.id)) return message.channel.send(`${Member.displayName} já foi silenciado.`).then((m) => m.delete({ timeout: 5000 }));
         await Member.roles.add(role2)
-        message.channel.send(`${Member.displayName} foi silenciado.`)
+        message.channel.send(`${Member.displayName} foi silenciado.`).then((m) => m.delete({ timeout: 5000 }));
 
         setTimeout(async () => {
             await Member.roles.remove(role2)
-            message.channel.send(`${Member.displayName} não está mais silenciado.`)
+            message.channel.send(`${Member.displayName} não está mais silenciado.`).then((m) => m.delete({ timeout: 5000 }));
         }, ms(time))
     }
 }
