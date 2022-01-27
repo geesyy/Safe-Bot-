8dfae4ce0c8b8b7cdb902c204bcbbae0df2b6f91
Gusstx19 <78054851+Gusstx19@users.noreply.github.com>
2022-01-27 00:39:15 -0300

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -3,14 +3,14 @@ const Discord = require("discord.js")
 exports.run = (bot, message, args) => {
   let user = message.mentions.users.first() || bot.users.cache.get(args[0])
     var membro = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
-    if (!membro) return message.reply('🚨 | O comando não foi digitado corretamente, digite *.helpadmin.*')
-    if (membro === message.member) return message.reply(`🚨 | Desculpe, mas você não tem permissão para isso.`)
+    if (!membro) return message.reply(`o comando não foi digitado corretamente, mencione o player.`).then((m) => m.delete({ timeout: 5000 }));
+    if (membro === message.member) return message.reply(`desculpe, mas você não tem permissão para isso.`).then((m) => m.delete({ timeout: 5000 }));
  
     var motivo = args.slice(1).join(" ");
-    if (!motivo) return message.channel.send(`🚨 | Motivo inválido.`)
-    if (!message.member.hasPermission("BAN_MEMBERS")) return message.reply(`🚨 | Desculpe, mas você não tem permissão para executar o comando.`)
-    if(!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send('🚨 | Desculpe, mas você não tem permissão executar o comando.')
-    if(!message.guild.me.hasPermission("BAN_MEMBERS")) return message.channel.send('🚨 | Desculpe, mas você não tem permissão para executar o comando.')
+    if (!motivo) return message.channel.send('Motivo inválido.')
+    if (!message.member.hasPermission("BAN_MEMBERS")) return message.reply(`desculpe, mas você não tem permissão para executar o comando.`).then((m) => m.delete({ timeout: 5000 }));
+    if(!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send(`desculpe, mas você não tem permissão executar o comando.`).then((m) => m.delete({ timeout: 5000 }));
+    if(!message.guild.me.hasPermission("BAN_MEMBERS")) return message.channel.send(`desculpe, mas você não tem permissão para executar o comando.`).then((m) => m.delete({ timeout: 5000 }));
  
 message.channel.send(`Deseja realmente banir o ${user}?  Clique no emoji.`).then(msg => {
         msg.react("👍")
